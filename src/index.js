65f287f280d5a288a1641fd76ca17798ebc79cac
Sergey Ladutko <verus774@mail.ru>
2018-02-25 15:15:24 +0300

    feature: implement solveEquation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    const [a, b, c] = parseEquation(equation);
+
+    const d = b**2 - 4*a*c;
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+    return ([Math.round(x1), Math.round(x2)].sort((a, b) => a - b));
+};
+
+function parseEquation(equationString) {
+    const myRegexp = /(-?\d+) \* x\^2 ([+-] \d+) \* x ([+-] \d+)/;
+    const match = myRegexp.exec(equationString);
+
+    const coefs = [];
+    for (let i = 1; i <=3; i++) {
+        coefs.push(parseInt(match[i].replace(/\s/g, ""), 10));
+    }
+
+    return coefs;
 }
